c770d8bfd0bc361b38a3e9d2876f59498696c003
Your Name <betancur10nicolas@gmail.com>
2022-10-12 17:31:06 -0500

    SR1

---
+++ POO.py
@@ -0,0 +1,10 @@
+class Personas:
+
+    def __init__(self, nombre, apellido, edad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+
+
+objecto = Personas("Nicolas", "betancur", 22)
+print(objecto.nombre)
